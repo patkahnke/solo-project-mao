f5f553fd6299fd06c62027b3fd5c233d34d5b82b
Patrick Kahnke <patrickkahnke@Patricks-MacBook-Pro.local>
2016-08-01 21:56:24 -0700

    update string rule

--- server/modules/rule.js
+++ server/modules/rule.js
@@ -87,34 +87,28 @@ function Rule() {
     return directionChangeCount++;
   };
 
-};
-
-Rule.prototype.stringNumbers = function (playedCard, targetCard) {
-  var cardNumber = parseInt(playedCard);
-  var targetCardNumber = parseInt(targetCard);
-  if (cardNumber == 13) {
-    if (targetCardNumber == 1 || targetCardNumber == 12) {
+  this.stringNumbers = function (playedCard, targetCard) {
+    var cardNumber = parseInt(playedCard);
+    var targetCardNumber = parseInt(targetCard);
+    if (cardNumber == 13) {
+      if (targetCardNumber == 1 || targetCardNumber == 12) {
+        return true;
+      } else {
+        return false;
+      }
+    } else if (cardNumber == 1) {
+      if (targetCardNumber == 2 || targetCardNumber == 13) {
+        return true;
+      } else {
+        return false;
+      }
+    } else if ((cardNumber + 1) == targetCardNumber || (cardNumber - 1) == targetCardNumber) {
       return true;
     } else {
       return false;
     }
-  } else if (cardNumber == 1) {
-    if (targetCardNumber == 2 || targetCardNumber == 13) {
-      return true;
-    } else {
-      return false;
-    }
-  } else if ((cardNumber + 1) == targetCardNumber || (cardNumber - 1) == targetCardNumber) {
-    return true;
-  } else {
-    return false;
-  }
-};
+  };
 
-function selectRandomRules(array1, array2) {
-  randomRule = array1[utility.randomNumber(0, array1.length - 1)];
-  array2.push(randomRule);
-  return array2;
-}
+};
 
 module.exports = Rule;
